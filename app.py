a26ade661f69006754136544aac2fe6eb7cec686
BobaFettyW4p <matthew.ivancic91@gmail.com>
2022-12-16 12:20:39 -0600

    current flask project

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templatefiles", static_folder="staticFiles")
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
